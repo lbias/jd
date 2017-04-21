a7b9ef5b9d1258028f9e360b766e7a7011cf4599
sam <morvazhu@gmail.com>
2017-04-21 19:44:49 +0800

    process resize_to_fit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -47,3 +47,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
 end
+
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
+
+  version :medium do
+    process resize_to_fill: [400,400]
+  end
